4a39cfec44937e65943f4f1b4abfff4e92cec301
kajkal <karolesniak@gmail.com>
2019-12-23 03:30:33 +0100

    Initial Commit

---
+++ Core_Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core_Data
+//
+//  Created by kjkl on 12/23/19.
+//  Copyright © 2019 kjkl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
