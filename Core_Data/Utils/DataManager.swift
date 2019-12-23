cb5a4bc1c3835a7c6f0d92b572194216e8e7b3d8
kajkal <karolesniak@gmail.com>
2019-12-23 03:57:30 +0100

    project setup

---
+++ Core_Data/Utils/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  Core_Data
+//
+//  Created by kjkl on 12/23/19.
+//  Copyright © 2019 kjkl. All rights reserved.
+//
+
+import Foundation
+
+class DataManager {
+    
+}
--- Core_Data/ViewController.swift
+++ Core_Data/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var dataManager: DataManager?
 
+    @IBOutlet weak var resultsLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dataManager = DataManager()
+    }
+    
+    private func printLog(message: String, startTime: NSDate) {
+        let measuredTime = abs(startTime.timeIntervalSinceNow)
+        let formattedTime = String(format: "%.4f", measuredTime)
+        let text = resultsLbl.text ?? ""
+        resultsLbl.text = text + "[\(formattedTime)s] \(message)\n"
     }
 
-
+    @IBAction func generateData(_ sender: UIButton) {
+        if let dataManager = self.dataManager {
+            let generateStartTime = NSDate()
+            
+        }
+    }
+    
+    @IBAction func queryMinMaxTime(_ sender: UIButton) {
+        if let dataManager = self.dataManager {
+            let generateStartTime = NSDate()
+            
+        }
+    }
+    @IBAction func queryAverageValue(_ sender: UIButton) {
+        if let dataManager = self.dataManager {
+            let generateStartTime = NSDate()
+            
+        }
+    }
+    @IBAction func querySensorAverageValue(_ sender: UIButton) {
+        if let dataManager = self.dataManager {
+            let generateStartTime = NSDate()
+            
+        }
+    }
+    
 }
 
